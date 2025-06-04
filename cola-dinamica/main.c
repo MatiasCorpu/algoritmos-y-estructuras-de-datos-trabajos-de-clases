1e73c1917e3f3495d2dda1a5d528eabb2d984572
MatiasCorpu <corpumatiasarial@gmail.com>
2025-06-04 19:56:03 -0300

    update main cola-dinamica

--- cola-dinamica/main.c
+++ cola-dinamica/main.c
@@ -4,5 +4,15 @@
 int main()
 {
 
-    return 0;
+    /*
+    void colaCrear(tCola *cola);
+    void colaVaciar(tCola *cola);
+    bool colaLlena(const tCola *cola, unsigned tamDato);
+    bool colaVacia(const tCola *cola);
+    int colaVerPrimero(const tCola *cola, void *dato, unsigned tamDato);
+    int colaSacarDato(tCola *cola, void *dato, unsigned tamDato);
+    int colaInsertarDato(tCola *cola, const void *dato, unsigned tamDato);
+    */
+
+    return TODO_OK;
 }
